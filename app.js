262256796efc5131dd4fa7abf451708ddf99f634
ughurba <ughurba@edu.code.az>
2022-05-24 17:31:47 +0400

    finish

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const link = document.querySelectorAll('.link1');
+
+
+
+let list = document.createElement('ul')
+document.body.append(list)
+
+
+
+let arr2 = Array.from(link).map(item => item.textContent)
+
+
+  arr2.reduce((sum, el)=>{
+    sum.set(el, (sum.get(el) || 0) +1);
+
+      if(sum.get(el) === 1){
+          let newLi = document.createElement('li')
+          newLi.innerText = el
+          list.append(newLi)
+      }
+
+      return sum
+},new Map())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
