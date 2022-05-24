5446a95785f254fa7fdfb4558acb38b09dbd9b19
ughurba <ughurba@edu.code.az>
2022-05-24 16:29:15 +0400

    I added checkbox task

--- checkbox.js
+++ checkbox.js
@@ -1,39 +1,39 @@
-const input = document.querySelector('.form-control')
-const btn = document.querySelector('.save')
-let list = document.querySelector('.list-group')
-const removeBtn = document.querySelector('.remove')
-
-
-
-btn.addEventListener('click',function (){
-    let check = document.createElement('input')
-    let link = document.createElement('li')
-    let inputValue = input.value.trim()
-
-
-        if(inputValue !== ''){
-            check.type='checkbox'
-            check.classList.add('form-check-input')
-
-            link.innerText = inputValue
-            link.classList.add('list-group-item')
-            input.value = ''
-            link.appendChild(check)
-            link.style.display ='flex'
-            link.style.justifyContent='space-between'
-            list.append(link)
-        }
-
-
-});
-
-removeBtn.addEventListener('click',function (){
- let   selectedCheckBoxes  = document.querySelectorAll('.form-check-input:checked')
-
-   selectedCheckBoxes.forEach(item => {
-       item.parentElement.remove()
-   })
-
-
-
-})
+// const input = document.querySelector('.form-control')
+// const btn = document.querySelector('.save')
+// let list = document.querySelector('.list-group')
+// const removeBtn = document.querySelector('.remove')
+//
+//
+//
+// btn.addEventListener('click',function (){
+//     let check = document.createElement('input')
+//     let link = document.createElement('li')
+//     let inputValue = input.value.trim()
+//
+//
+//         if(inputValue !== ''){
+//             check.type='checkbox'
+//             check.classList.add('form-check-input')
+//
+//             link.innerText = inputValue
+//             link.classList.add('list-group-item')
+//             input.value = ''
+//             link.appendChild(check)
+//             link.style.display ='flex'
+//             link.style.justifyContent='space-between'
+//             list.append(link)
+//         }
+//
+//
+// });
+//
+// removeBtn.addEventListener('click',function (){
+//  let   selectedCheckBoxes  = document.querySelectorAll('.form-check-input:checked')
+//
+//    selectedCheckBoxes.forEach(item => {
+//        item.parentElement.remove()
+//    })
+//
+//
+//
+// })
