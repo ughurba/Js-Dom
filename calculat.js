81324e32547c3767f3082b5778455797510e25af
ughurba <ughurba@edu.code.az>
2022-05-24 16:29:25 +0400

    I added calculat task

---
+++ calculat.js
@@ -0,0 +1,57 @@
+// const  inputOne = document.querySelector('.inputOne')
+// const  inputTwo = document.querySelector('.inputTwo')
+// const plus = document.querySelector('.plus')
+// const minus = document.querySelector('.minus')
+// const multiply = document.querySelector('.multiply')
+// const divide = document.querySelector('.divide')
+// const sum = document.querySelector('.sum')
+//
+// // let getCalcTool = function (){
+// //
+// // let local_math ={
+// //
+// //   "+": (a, b) => a + b,
+// //   "-": (a, b) => a - b,
+// //   "/": (a, b) => a / b,
+// //   "*": (a, b) => a * b,
+// // }
+// // return function (a,operator,b){
+// //
+// //   return local_math[operator](a,b);
+// //
+// //   }
+// //
+// //   sum.value =  inputOne.value   (inputTwo.value)
+// //   inputTwo.value=''
+// //   inputOne.value=''
+// // }
+//
+// plus.addEventListener('click', function (){
+//
+//   sum.value =  Number(inputOne.value) + Number(inputTwo.value)
+//   inputTwo.value=''
+//   inputOne.value=''
+// })
+//
+// minus.addEventListener('click', function (){
+//
+//
+//   sum.value =  inputOne.value - inputTwo.value
+//   inputTwo.value=''
+//   inputOne.value=''
+// })
+//
+// multiply.addEventListener('click', function (){
+//
+//   sum.value =  inputOne.value * inputTwo.value
+//   inputTwo.value=''
+//   inputOne.value=''
+// })
+//
+//
+// divide.addEventListener('click', function (){
+//
+//   sum.value =  inputOne.value / inputTwo.value
+//   inputTwo.value=''
+//   inputOne.value=''
+// })
