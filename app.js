83d8b79be83033d4b99e6db695034f9038671214
ughurba <ughurba@edu.code.az>
2022-05-24 17:38:04 +0400

    fix-bug

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 
 const link = document.querySelectorAll('.link1');
+const  btn = document.querySelector('.newListClick')
 
 
 
 let list = document.createElement('ul')
-document.body.append(list)
+
+btn.addEventListener('click',function (){
+
+    document.body.append(list)
 
 
 
-let arr2 = Array.from(link).map(item => item.textContent)
+    let arr2 = Array.from(link).map(item => item.textContent)
 
 
-  arr2.reduce((sum, el)=>{
-    sum.set(el, (sum.get(el) || 0) +1);
+    arr2.reduce((sum, el)=>{
+        sum.set(el, (sum.get(el) || 0) +1);
 
-      if(sum.get(el) === 1){
-          let newLi = document.createElement('li')
-          newLi.innerText = el
-          list.append(newLi)
-      }
+        if(sum.get(el) === 1){
+            let newLi = document.createElement('li')
+            newLi.innerText = el
+            list.append(newLi)
+        }
 
-      return sum
-},new Map())
+        return sum
+    },new Map())
+
+
+})
+
 
 
 
